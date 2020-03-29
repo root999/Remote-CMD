03ffb210395815c4fff1f21e0697f9a73aec007e
root999 <cagriiikilic@gmail.com>
2020-03-29 03:33:13 +0300

    argument parser active

--- serverupgrade.c
+++ serverupgrade.c
@@ -8,13 +8,94 @@
 #include <sys/wait.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <argp.h>
 #define BACKLOG 10
 
-int main(int argc, char const *argv[]){
+int arg_number =0;
+static char args_doc[] = "Enter port value, username and password";
+static struct argp_option options[] = {
+  {"port",'s',"socket",OPTION_ARG_OPTIONAL,"Defines port number. Default:4123" },
+  
+  {"user",'u', "user",0,"Defines authorized user. Default:user" },
+  
+  {"pw",'p',"pass",0,"Defines password of the user. Default:pass" },
+  
+  { 0 }
+};
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+  char *args[3];                /* arg1 & arg2 */
+  int port;
+  char username[20], password[20];
+};
+
+/* Parse a single option. */
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+   
+  switch (key)
+    {
+    case 's':
+      //printf("in s: %s",arg);
+      arguments->port = atoi(arg);
+      arg_number++;
+      break;
+    case 'u':
+    //printf("in u: %s",arg);
+      strcpy(arguments->username,arg);
+      arg_number++;
+      break;
+    case 'p':
+    //printf("in p: %s",arg);
+     strcpy(arguments->password,arg);
+     arg_number++;
+      break;
+
+    case ARGP_KEY_ARG:
+    printf("\n%d\n",arg_number);
+      if (arg_number >= 4){
+          argp_usage (state);
+          arguments->args[state->arg_num] = arg;
+      }
+        /* Too many arguments.*/
+      break;
+
+    case ARGP_KEY_END:
+      if (arg_number < 3){
+          argp_usage (state);
+      }
+        /* Not enough arguments. */
+        
+      break;  
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = { options, parse_opt, args_doc,doc};
+
+int main(int argc, char **argv){
+
+    struct arguments arguments;
+    //printf("first");
+    /* Default values. */
+    strcpy(arguments.username,"user");
+    strcpy(arguments.password,"pass");
+    arguments.port = 4123;
+    argp_parse (&argp, argc, argv, 0, 0, &arguments);
+    printf("\nu:%s pass: %s  port:%d",arguments.username,arguments.password,arguments.port);
+    fflush(stdout);
     int sockfd, new_fd;
-    int portN = 4123 ;
-    char *cmd_args[] = {"ls", "-l", NULL};
-    char *cmd;
+    int portN = arguments.port ;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     socklen_t sin_size = sizeof(struct sockaddr_in);;
@@ -55,6 +136,22 @@ int main(int argc, char const *argv[]){
             waitpid(child_pid,NULL,0);
         }
         else {
+            char cmd_args[50]; 
+            char cmd[50];
+            char cmd_arg[50];
+            dup2(new_fd, 1); /* Duplicate socket's FD over FD #1, aka stdout */
+            dup2(new_fd, 0); /* Duplicate socket's FD over FD #0, aka stdin */
+            close(new_fd); /* Only closes the child's copy.  And the stdin/stdout copies remain. */
+            //execlp("/bin/echo", "HEY GUYS ALJ AF MY FACE IS A ROTTORN BANANA");
+            fprintf(stdout,"denemedir bu \n");
+            fflush(stdout);
+            fscanf(stdin,"%s",cmd);
+            fscanf(stdin,"%s",cmd_args);
+            fscanf(stdin,"%s",cmd_arg);
+            execlp(cmd,cmd_args,cmd_arg,NULL);
+            //execvp( cmd, cmd_args );
+            perror("the execvp(3) call failed.");
+            exit(1);
             // auth client
             // start connection handler
            
@@ -72,6 +169,9 @@ return 0;
 }
 
 int conn_handl(int connsck){
+    char cmd_args[50]; 
+    char cmd[50];
+    char cmd_arg[50];
     dup2(connsck, 1); /* Duplicate socket's FD over FD #1, aka stdout */
     dup2(connsck, 0); /* Duplicate socket's FD over FD #0, aka stdin */
     close(connsck); /* Only closes the child's copy.  And the stdin/stdout copies remain. */
